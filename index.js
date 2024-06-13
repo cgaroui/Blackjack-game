7a3ac85b76275b9b407106418e35e39071d11584
chaima garoui <chaimagaroui2@gmail.com>
2024-06-13 19:15:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+let firstCard = 6
+let secondCard = 9
+let sum = firstCard + secondCard
+
+if (sum < 21) {
+    console.log("do you want to draw a new card ?")
+}
+else if(sum == 21) {
+    console.log("Wohouu! you've got Blackjack!")
+}
+
+else  {
+    console.log("tou're out of the game!")
+}
+
+//-----------------------------------------
+
+//check if the person is elegible dor a birthday card from the King! (100)
+
+let age = 100
+
+if (age < 100) {
+        console.log("Not elegible")
+}
+else if ( age === 100){
+    console.log("here is your birthday card from the King!")
+}else {
+    console.log("Not elegible! you have already gotten one ")
+}
